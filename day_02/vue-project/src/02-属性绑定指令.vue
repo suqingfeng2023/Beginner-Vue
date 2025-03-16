9873793777c5799db7a10be5a8cae6a67752d932
suqingfeng2023 <1079791734@qq.com>
2025-03-16 22:57:30 +0800

    属性绑定指令

---
+++ day_02/vue-project/src/02-属性绑定指令.vue
@@ -0,0 +1,23 @@
+<!-- 属性绑定指令 -->
+
+<!-- 
+v-bind:属性名="表达式"
+-->
+
+<template>
+    <div v-bind:title="msg">
+        {{ msg }}
+    </div>
+    <a :href="url", >百度一下</a>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    const msg = ref('Hello world');
+    const url = "www.baidu.com"
+</script>
+
+<style scoped>
+
+</style>
