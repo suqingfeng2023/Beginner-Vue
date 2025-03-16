5c5a9fe10d90c934242150661ca40971a0569a4b
suqingfeng2023 <1079791734@qq.com>
2025-03-16 21:53:31 +0800

    Vue标签的学习

---
+++ day_02/vue-project/src/01-内容渲染指令.vue
@@ -0,0 +1,23 @@
+<!-- 内容渲染指令 -->
+ <!-- 
+    v-html="表达式"
+    v-text="表达式"
+ -->
+
+<template>
+<div>
+    <p v-html="str"></p>
+    <p v-text="str"></p>
+</div>
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+    const str = ref('<span style="color:red;">Hello Vue3+Vite</span>')
+    
+</script>
+
+<style scoped>
+
+</style>
+
