f6459890f4ba09f3f92e490c14eaf13c21b9a3b0
suqingfeng2023 <1079791734@qq.com>
2025-03-16 21:18:58 +0800

    第一个Vue程序

---
+++ day_01/vite-project/src/components/demo1.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+      <h2>{{ message }}</h2>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    setup() {
+      const message = ref('莫听穿林打叶声，何妨吟啸且徐行。');
+
+        setTimeout(() => {
+        message.value = '—— 苏轼';
+      }, 2000);
+  
+      return {
+        message,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  h2 {
+    color: blue;
+    font-family: "微软雅黑";
+    text-align: center;
+  }
+  </style>
