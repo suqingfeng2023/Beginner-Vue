d67a3d527f3ae7f930c5de9a7dd9f378046dd172
suqingfeng2023 <1079791734@qq.com>
2025-03-19 18:45:20 +0800

    课下作业_03_19

---
+++ day_03/vue-project/src/App.vue
@@ -0,0 +1,51 @@
+<!-- v-show 指令示例 -->
+
+<template>
+  <div>
+    <h1>v-show 指令示例</h1>
+    <p v-show="showMessage">这是一个显示的消息。</p>
+    <p v-show="!showMessage">这是一个隐藏的消息。</p>
+    <button @click="toggleMessage">切换消息</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// 定义响应式变量
+const showMessage = ref(true);
+
+// 定义切换方法
+const toggleMessage = () => {
+  showMessage.value = !showMessage.value;
+};
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+}
+
+h1 {
+  color: white;
+}
+
+p {
+  font-size: 20px;
+  margin: 20px;
+}
+
+button {
+  padding: 10px;
+  font-size: 16px;
+  color: black;
+  background-color: #dcd85b;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #3aa876;
+}
+</style>
