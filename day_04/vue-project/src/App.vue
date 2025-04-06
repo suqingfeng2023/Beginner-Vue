9796e606d8fdab4f6c38411bbd28e683db202db3
suqingfeng2023 <1079791734@qq.com>
2025-04-06 23:00:06 +0800

    vue学习

--- day_04/vue-project/src/App.vue
+++ day_04/vue-project/src/App.vue
@@ -1,52 +1,32 @@
 <script setup>
 import { ref } from 'vue'
-const tabs = ref([
-  {id: 1, name: '京东秒杀'},
-  {id: 2, name: '每日特价'},
-  {id: 3, name: '品类秒杀'}
-])
-const activeIndex = ref(0)
 
+const selectedOption = ref('自习室') // 默认选中值
+
+const options = ref([
+  { value: 'classroom', label: '自习室' },
+  { value: 'library', label: '图书馆' },
+  { value: 'dorm', label: '宿舍' }
+])
 </script>
 
 <template>
-  <ul>
-    <li v-for="(item, index) in tabs" :key="index" @click="activeIndex = index">
-      <!-- 这里的activeIndex是一个响应式数据，点击li时会改变activeIndex的值 -->
-      <a 
-      href="#"
-      :class="{active: activeIndex == index}">{{ item.name }}
-      </a>
-    </li>
-  </ul>
+  <select 
+    class="form-select form-select-sm mb-3" 
+    v-model="selectedOption"
+  >
+    <option 
+      v-for="option in options" 
+      :key="option.value"
+      :value="option.value"
+    >
+      {{ option.label }}
+    </option>
+  </select>
+  
+  <div>当前选择：{{ selectedOption }}</div>
 </template>
 
 
-<style scoped>
-* {
-  margin: 0;
-  padding: 0;
-}
 
-ul {
-  display: flex;
-  list-style: none;
-  border-bottom: 1px solid rgb(158, 9, 9);
-}
-ul li {
-  width: 100px;
-  height: 50px;
-  text-align: center;
-  line-height: 50px;
-}
-ul li a {
-  display: block;
-  text-decoration: none;
-  color: #000;
-  font-weight: bolder;
-}
-ul li a.active {
-  background-color: rgb(193, 38, 38);
-  color: #fff;
-}
-</style>
+<style scoped></style>
