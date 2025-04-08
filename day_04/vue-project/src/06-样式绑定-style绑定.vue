883d0c468026b29f550b3eafcb0a543852f38330
suqingfeng2023 <1079791734@qq.com>
2025-04-09 00:52:23 +0800

    Vue学习

---
+++ day_04/vue-project/src/06-样式绑定-style绑定.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { reactive, ref } from 'vue'
+// 字体颜色
+const colorStr = ref('red')
+// 响应式样式对象
+const styleObj = reactive({
+  color: 'red',
+  fontSize: '20px',
+  backgroundColor: 'blue'
+})
+</script>
+
+<template>
+  <div>
+    <p :style="{ color : colorStr }">style1</p>
+    <p :style="styleObj">style2</p>
+  </div>
+
+</template>
+
+
+
+<style scoped>
+
+</style>
