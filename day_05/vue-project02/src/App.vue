77f548e73e388da885e6b9d5a5fb9139fc37efa5
suqingfeng2023 <1079791734@qq.com>
2025-04-14 11:52:03 +0800

    Vue组件的命名方式

---
+++ day_05/vue-project02/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+// 导入组件
+import MyPanel from './components/MyPanel.vue';
+</script>
+<template>
+  <h3>可折叠⾯板</h3>
+  <MyPanel />
+  <MyPanel></MyPanel>
+  <my-panel></my-panel>
+</template>
+<style lang="scss">
+body {
+  background-color: #ccc;
+}
+
+#app {
+  width: 400px;
+  margin: 20px auto;
+  padding: 1em 2em 2em;
+  border: 4px solid green;
+  border-radius: 1em;
+  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
+  background-color: #fff;
+}
+
+#app h3 {
+  text-align: center;
+}
+
+
+</style>
